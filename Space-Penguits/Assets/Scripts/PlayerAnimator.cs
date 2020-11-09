cd3de2d66e9b3216d21e7c21ed71764ce9090a7d
jnromeroa <nico981006@gmail.com>
2020-11-09 01:15:11 -0500

    primer prototipo semifuncional
    arregle el codigo del player, añadi animator y ajuste animaciones. falta que siempre al chocar otro planeta quede de pie

---
+++ Space-Penguits/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator playerAnim;
+    void Start()
+    {
+        playerAnim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerAnim.SetBool("Grounded",GetComponent<Player>().grounded);
+    }
+}
