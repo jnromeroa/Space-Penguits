de54fda9519ec95751a95e5fdbf9c900a585c16f
jnromeroa <nico981006@gmail.com>
2020-12-03 12:56:51 -0500

    arreglé script de camara
    camara ahora se mueve perfectamente, agregué efecto parallax pero falta arreglarlo

--- Space-Penguits/Assets/Scripts/Planetas.cs
+++ Space-Penguits/Assets/Scripts/Planetas.cs
@@ -23,27 +23,19 @@ public class Planetas : MonoBehaviour
     {
         GetComponent<Transform>().Rotate(0, 0, velocidadGiro);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            
-        }
-    }
+    
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             Physics2D.gravity = transform.position - collision.gameObject.transform.position;
-            
-            CamaraRig.transform.position = Vector3.MoveTowards(CamaraRig.transform.position, 
-                new Vector3((transform.position.x+nextPlanet.transform.position.x)/2,
-                (transform.position.y + nextPlanet.transform.position.y) / 2), velocidadCamara * Time.deltaTime);
+
+            CamaraRig.GetComponent<CameraMovement>().TowardsNextPlanet(transform.position, nextPlanet.transform.position);
+            /*CamaraRig.transform.position = transform.Translate(
+            new Vector2((transform.position.x + nextPlanet.transform.position.x) / 2,
+            (transform.position.y + nextPlanet.transform.position.y) / 2));*/
             
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        
-    }
+    
 }
