ee6fb2d9f8184cf75d6792d0b4eb1a96f80377f0
jnromeroa <nico981006@gmail.com>
2020-12-06 16:40:14 -0500

    termine opciones
    perfeccione opciones y lo enlace a menu

---
+++ Space-Penguits/Assets/Scripts/AudioVol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioVol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mislider;
+    void Start()
+    {
+        DontDestroyOnLoad(mislider.gameObject);
+        AudioListener.volume = mislider.GetComponent<Slider>().value;
+        Debug.Log(AudioListener.volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = mislider.GetComponent<Slider>().value;
+        Debug.Log(AudioListener.volume);
+    }
+}
