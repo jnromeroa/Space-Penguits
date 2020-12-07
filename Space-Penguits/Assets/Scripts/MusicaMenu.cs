b892cb43e6a7cfba02923a95f9a5b569b492dc9e
jnromeroa <nico981006@gmail.com>
2020-12-07 08:22:03 -0500

    arregle musica menu

---
+++ Space-Penguits/Assets/Scripts/MusicaMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicaMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static MusicaMenu MMInstance;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (MMInstance == null)
+        {
+
+            MMInstance = this;
+
+
+
+        }
+        else
+        {
+            Object.Destroy(gameObject);
+            
+
+
+
+        }
+    }
+}
