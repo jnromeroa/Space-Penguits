ed16e6ea9132f5431249b58702708dd4c6fca023
jnromeroa <nico981006@gmail.com>
2020-12-06 23:05:02 -0500

    cambie ubicacion anims
    ademas cree intro y le agregue anims

---
+++ Space-Penguits/Assets/Scripts/EventManagerIntro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+public class EventManagerIntro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject textoIntro;
+    [SerializeField] private GameObject panel;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            textoIntro.GetComponentInChildren<Animator>().SetBool("Clicked", true);
+            panel.GetComponentInChildren<Animator>().SetBool("Clicked", true);
+        }
+    }
+}
